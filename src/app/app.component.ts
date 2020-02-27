ee72f93778cb73ad6d9a5a86b86493c5646b3786
garygrossgarten <gary@fivethree.io>
2020-02-27 03:25:44 +0100

    fouc?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private platform: Platform) {
+    this.platform.ready().then(() => {
+      document.body.className = document.body.className.replace('fouc', '');
+    });
+  }
+}
