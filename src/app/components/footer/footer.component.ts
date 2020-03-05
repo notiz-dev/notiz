d1aade2dc488a377b8b4286fa5fa9ec1f5da4998
Marc Stammerjohann <marcstammerjohann@web.de>
2020-03-05 20:02:38 +0100

    feat(copyright): use date now for copyright

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  now = Date.now();
+
   constructor() {}
 
   ngOnInit(): void {}
