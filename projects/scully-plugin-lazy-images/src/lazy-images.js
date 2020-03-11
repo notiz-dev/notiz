11fc13608175da4634084d32a370e363899dadda
garygrossgarten <gary@fivethree.io>
2020-03-12 00:59:34 +0100

    cross browser support

--- projects/scully-plugin-lazy-images/src/lazy-images.js
+++ projects/scully-plugin-lazy-images/src/lazy-images.js
@@ -4,9 +4,37 @@ const lazyImagesPlugin = async (html, route) => {
   const dom = new JSDOM(html);
   const doc = dom.window.document;
   var imgEl = doc.getElementsByTagName('img');
+
+  // can be added when loadine="lazy" is supported in more browsers
+  //   for (var i = 0; i < imgEl.length; i++) {
+  //     imgEl[i].setAttribute('loading', 'lazy');
+  //   }
+
   for (var i = 0; i < imgEl.length; i++) {
-    imgEl[i].setAttribute('loading', 'lazy');
+    if (imgEl[i].getAttribute('src')) {
+      imgEl[i].setAttribute('data-src', imgEl[i].getAttribute('src'));
+      imgEl[i].removeAttribute('src');
+      imgEl[i].classList.add('lazyload');
+    }
   }
+
+  const lib = doc.createElement('script');
+  lib.src = 'https://cdn.jsdelivr.net/npm/lazyload@2.0.0-rc.2/lazyload.js';
+  const s = doc.createElement('script');
+  s.innerHTML = `
+    (() => { 
+      document.addEventListener('readystatechange',function(){
+          if(document.readyState === 'complete'){
+              setTimeout(() => {
+                lazyload();
+              },0)
+          }
+      })
+    })();
+    `;
+  doc.body.append(lib);
+  doc.body.append(s);
+
   return dom.serialize();
 };
 
