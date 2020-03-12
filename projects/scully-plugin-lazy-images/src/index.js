d543634d6c417c9f0b18f8e416b8a9d72e95e0ed
Marc Stammerjohann <marcstammerjohann@web.de>
2020-03-12 11:51:40 +0100

    feat(lazy-images): install lazy-images from npm

--- projects/scully-plugin-lazy-images/src/index.js
+++
@@ -1,5 +0,0 @@
-const { registerPlugin } =  require('@scullyio/scully');
-const { lazyImagesPlugin } =  require('./lazy-images');
-
-const validator = async () => [];
-registerPlugin('render', 'lazyImages', lazyImagesPlugin, validator);
--- scully.notiz.config.js
+++ scully.notiz.config.js
@@ -1,17 +1,17 @@
-require('./projects/scully-plugin-lazy-images');
+require('@notiz/scully-plugin-lazy-images');
 require('./projects/scully-plugin-fouc');
 
 exports.config = {
   projectRoot: './src',
   projectName: 'notiz',
-  defaultPostRenderers: ['lazyImages','fouc'],
+  defaultPostRenderers: ['lazyImages', 'fouc'],
   outDir: './dist/static',
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
       slug: {
         folder: './content/blog'
-      },
+      }
     },
     '/authors/:slug': {
       type: 'contentFolder',
