3c49e3494db6dc7465eb3b213652b41f3462dca3
garygrossgarten <gary@fivethree.io>
2020-03-13 01:27:14 +0100

    added rss plugin, removed local plugin projects

--- projects/scully-plugin-fouc/src/index.js
+++
@@ -1,5 +0,0 @@
-const { registerPlugin } = require('@scullyio/scully');
-const { foucPlugin } = require('./fouc');
-
-const validator = async () => [];
-registerPlugin('render', 'fouc', foucPlugin, validator);
--- scully.notiz.config.js
+++ scully.notiz.config.js
@@ -1,17 +1,29 @@
 require('@notiz/scully-plugin-lazy-images');
-require('./projects/scully-plugin-fouc');
+require('@notiz/scully-plugin-fouc');
+require('@notiz/scully-plugin-rss');
 
 exports.config = {
   projectRoot: './src',
   projectName: 'notiz',
   defaultPostRenderers: ['lazyImages', 'fouc'],
+  rssConfig: {
+    title: 'title',
+    description: 'description',
+    feed_url: 'https://notiz.dev/feed.xml',
+    site_url: 'https://notiz.dev',
+    image_url: 'https://notiz.dev/assets/img/featured.png',
+    copyright: '2020 notiz.dev',
+    language: 'en',
+    categories: ['Web Development', 'Angular', 'node', 'Programming Blog'],
+  },
   outDir: './dist/static',
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
       slug: {
         folder: './content/blog'
-      }
+      },
+      postRenderers: ['lazyImages', 'fouc', 'rss']
     },
     '/authors/:slug': {
       type: 'contentFolder',
