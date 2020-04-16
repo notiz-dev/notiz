fe3b93ed8599a12ed6d0df369c7e2af16ce8867f
garygrossgarten <gary@fivethree.io>
2020-04-16 16:59:50 +0200

    add limit

--- src/app/components/tags-view/tags-view.component.ts
+++ src/app/components/tags-view/tags-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ScullyRoutesService, ScullyRoute } from '@scullyio/ng-lib';
-import { map, switchMap, tap, reduce } from 'rxjs/operators';
+import { ScullyRoute } from '@scullyio/ng-lib';
+import { map, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { ScullyContentService } from '@services/scully-content.service';
 import { TagWeight } from '../../types/types';
@@ -13,15 +13,23 @@ import { TagWeight } from '../../types/types';
 export class TagsViewComponent implements OnInit {
   @Input() blogPosts: Observable<ScullyRoute[]>;
   @Input() tags: Observable<ScullyRoute[]>;
+  @Input() limit;
+  count: number;
 
   weightedTags$: Observable<TagWeight[]>;
 
   constructor(private scullyContent: ScullyContentService) {}
 
   ngOnInit(): void {
-    this.weightedTags$ = this.scullyContent.weightedTags(
-      this.blogPosts,
-      this.tags
-    );
+    this.weightedTags$ = this.scullyContent
+      .weightedTags(this.blogPosts, this.tags)
+      .pipe(
+        map(tags => tags.sort((t1, t2) => (t1.weight < t2.weight ? 1 : -1))),
+        tap(tags => (this.count = tags.length)),
+        map(tags => tags.slice(0, (this.limit || this.count))),
+        map(tags =>
+          tags.sort((t1, t2) => (t1.tag.title > t2.tag.title ? 1 : -1))
+        )
+      );
   }
 }
