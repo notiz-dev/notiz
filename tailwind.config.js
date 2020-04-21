73bb3da7b8cda4999113b09b698cd986057724dd
Marc Stammerjohann <marcstammerjohann@web.de>
2020-04-21 16:36:38 +0200

    style: update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,11 @@ module.exports = {
     extend: {
       colors: {
         blue: 'var(--blue)',
+        'blue-opacity-10': 'rgba(var(--blue-rgb), 0.1)',
         cyan: 'var(--cyan)',
+        'cyan-opacity-10': 'rgba(var(--cyan-rgb), 0.1)',
         'dark-blue': 'var(--dark-blue)',
+        'dark-blue-opacity-10': 'rgba(var(--dark-blue-rgb), 0.1)',
         background: 'var(--background)',
         color: 'var(--text-color)',
         'color-light': 'var(--text-color-light)',
