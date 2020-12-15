f0408fb12d884ac09d97087ff4fa950ba0709e70
Marc Stammerjohann <marcstammerjohann@web.de>
2020-12-15 16:43:37 +0100

    update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,18 @@ module.exports = {
       white: '#fff',
       red: colors.red,
       green: colors.green,
-      gray: { ...colors.coolGray, 950: '#141922' },
+      gray: {
+        100: '#f7fafc',
+        200: '#edf2f7',
+        300: '#e2e8f0',
+        400: '#cbd5e0',
+        500: '#a0aec0',
+        600: '#718096',
+        700: '#4a5568',
+        800: '#2d3748',
+        900: '#1a202c',
+        950: '#141922',
+      },
       primary: {
         light: '#55f3de',
         DEFAULT: '#55b9f3',
@@ -88,7 +99,10 @@ module.exports = {
             h6: { color: theme('colors.color') },
             strong: { color: theme('colors.color') },
             'ul li:before': {
-              backgroundColor: theme('colors.primary.DEFAULT'),
+              backgroundColor: theme('colors.color-light'),
+            },
+            'ol li:before': {
+              color: theme('colors.color-light'),
             },
             a: {
               color: theme('colors.primary.DEFAULT'),
@@ -112,19 +126,19 @@ module.exports = {
             },
             'code::before': { content: '""' },
             'code::after': { content: '""' },
+            pre: {
+              color: theme('colors.color'),
+              backgroundColor: theme('colors.background-light'),
+            },
             blockquote: {
               color: theme('colors.color'),
-              borderLeftColor: theme('colors.primary.DEFAULT'),
+              borderLeftColor: theme('colors.color-light'),
             },
             'blockquote p:first-of-type::before': {
               content: '""',
             },
             'blockquote p:last-of-type::after': {
               content: '""',
-            },
-            pre: {
-              color: theme('colors.color'),
-              backgroundColor: theme('colors.background-light'),
             },
           },
         },
