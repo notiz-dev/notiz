7e8ff2216f5c0325e32a14add311e7505cd221dc
Marc Stammerjohann <marcstammerjohann@web.de>
2020-12-30 16:41:51 +0100

    fix(series): move chaperts into its own div
    * scully-content needs its own div otherwise *ngIf="page$ | async as page" is not correctly interpreted

--- src/app/pages/series-details/series-details.component.ts
+++ src/app/pages/series-details/series-details.component.ts
@@ -1,31 +1,23 @@
 import { ScullyContentService } from '@services/scully-content.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ScullyRoute } from '@scullyio/ng-lib';
-import { Observable, Subject } from 'rxjs';
-import { takeUntil, switchMap } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'niz-series-details',
   templateUrl: './series-details.component.html',
   styleUrls: ['./series-details.component.scss'],
 })
-export class SeriesDetailsComponent implements OnInit, OnDestroy {
-  page$: Observable<ScullyRoute> = this.scullyContent.getCurrent();
+export class SeriesDetailsComponent {
+  page$: Observable<ScullyRoute> = this.scully.getCurrent();
 
-  seriesPosts$: Observable<ScullyRoute[]>;
+  seriesPosts$: Observable<ScullyRoute[]> = this.page$.pipe(
+    switchMap((page) => this.scullyContent.seriesPosts(page))
+  );
 
-  private destroy$ = new Subject();
-
-  constructor(private scullyContent: ScullyContentService) {}
-
-  ngOnInit(): void {
-    this.seriesPosts$ = this.page$.pipe(
-      switchMap((page) => this.scullyContent.seriesPosts(page))
-    );
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
+  constructor(
+    private scullyContent: ScullyContentService,
+    private scully: ScullyRoutesService
+  ) {}
 }
