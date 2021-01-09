d9aedb3d4122ceaea600b1eb077d78c371f0e9eb
Marc Stammerjohann <marcstammerjohann@web.de>
2021-01-09 18:32:55 +0100

    styles: prose newsletter text and add link to tags

--- src/app/components/newsletter-signup/newsletter-signup.module.ts
+++ src/app/components/newsletter-signup/newsletter-signup.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsletterSignupComponent } from './newsletter-signup.component';
@@ -13,6 +14,7 @@ import {
   imports: [
     CommonModule,
     FormsModule,
+    RouterModule,
     ReactiveFormsModule,
     NizPrimaryButtonModule,
     NizInputModule,
