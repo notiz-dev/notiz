15fe7027a73541fcdb46ee36577dd5a87de636d1
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-10-27 22:16:56 +0200

    remove prism theme, add comment, boolen and attr-value for prisma

--- src/app/services/prism-prisma.js
+++ src/app/services/prism-prisma.js
@@ -1,6 +1,9 @@
-(function(Prism) {
+(function (Prism) {
   Prism.languages.prisma = {
+    comment: /\/\/\/?.*/,
     keyword: /\b(?:datasource|generator|model|enum)\b/,
-    builtin: /\b(?:String|Int|Boolean|DateTime)\b/
+    builtin: /\b(?:String|Int|Boolean|DateTime)\b/,
+    boolean: /\b(true|false)\b/,
+    'attr-value': /(@@?[\w\.]+)/,
   };
 })(Prism);
