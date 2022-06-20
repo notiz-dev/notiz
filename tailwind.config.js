ee6fbec240d24a29cd2277e443f03a2c04388ad3
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2022-06-20 10:19:56 +0200

    add tailwind types

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const withOpacity = require('./tailwind/with-opacity');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/**/*.{html,ts}',
